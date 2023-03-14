1c58eaebb8a39138826cd0c7a3618ff28ebc87d6
amritagoswami <amritagoswami30@gmail.com>
2023-03-14 22:25:07 +0100

    Corrected the db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require "open_uri"
+require "open-uri"
 
 Listing.destroy_all
 User.destroy_all
 
-user_seed = User.create(first_name "Amrita", last_name "Goswami", email: "amritagg@gmail.com", password: "123456")
-user_seed = User.create(first_name "Abc", last_name "Efg", email: "stmh@gmail.com", password: "143567")
+user_seed = User.create(first_name: "Amrita", last_name: "Goswami", email: "amritagg@gmail.com", password: "123456")
 
 file = URI.open("https://storage.kempinski.com/cdn-cgi/image/w=1920,f=auto,g=auto,fit=scale-down/ki-cms-prod/images/1/9/8/6/86891-1-eng-GB/5b43edfee48c-73660559_4K.jpg")
 first_seed = Listing.new(
   listing_name: 'Basecamp',
   listing_address: 'Halden, Norway',
   listing_amenities: 'WiFi',
-  listing_description: 'Centrally located in Longyearbyen, Basecamp Hotel is a traditional trappers lodge-style property just 15 minutes drive from Longyearbyen Airport.',
+  listing_description: 'Centrally located in Longyearbyen, Basecamp Hotel is a traditional trappers lodge-style property.'
 )
 # main picture
 first_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
@@ -26,14 +25,17 @@ first_seed.user = user_seed
 first_seed.save
 
 
+
+
 file = URI.open("https://plus.unsplash.com/premium_photo-1678286771657-cf22aa97faf0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60.jpg")
-second_seed =Listing.new(
-    listing_name: 'Basecamp',
-    listing_address: 'Halden, Norway',
-    listing_amenities: 'Free Wi_Fi, TV with cable'
-    listing_description: 'Centrally located in Longyearbyen, Basecamp Hotel is a traditional trapper's lodge-style property just 15 minutes’ drive from Longyearbyen Airport.
-  )
-  second_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+second_seed = Listing.new(
+  listing_name: 'Clarion',
+  listing_address: 'Kongsberg, Norway',
+  listing_amenities: 'Free Wi_Fi, TV with cable',
+  listing_description: 'Centrally located in Kongsberg,15 minutes away from tog station.'
+)
+
+   second_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
   second_seed.user = user_seed
   second_seed.save
 
@@ -42,7 +44,7 @@ second_seed =Listing.new(
   third_seed =Listing.new(
     listing_name: 'Svalbard',
     listing_address: 'Halden, Norway',
-    listing_amenities: 'Free Wi_Fi, TV with cable'
+    listing_amenities: 'Free Wi_Fi, TV with cable',
     listing_description: 'Located in Longyearbyen, 1.1 km from Svalbard Church, Svalbard Hotell | The Vault provides accommodation with free bikes, free private parking, a shared lounge and a terrace.'
   )
   third_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
@@ -55,8 +57,8 @@ second_seed =Listing.new(
   fourth_seed =Listing.new(
     listing_name: 'Funken',
     listing_address: 'Halden, Norway',
-    listing_amenities: 'Free Wi_Fi, TV with cable'
-    listing_description: 'This hotel is situated on the island of Spitsbergen, in Norway’s Arctic Archipelago, Svalbard.'
+    listing_amenities: 'Free Wi_Fi, TV with cable',
+    listing_description: 'This hotel is situated on the island of Spitsbergen, in Norway Arctic Archipelago, Svalbard.'
   )
   fourth_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
   fourth_seed.user = user_seed
@@ -67,22 +69,20 @@ second_seed =Listing.new(
   fifth_seed =Listing.new(
     listing_name: 'Radisson Blu',
     listing_address: 'Oslo, Norway',
-    listing_amenities: 'Free Wi_Fi, TV with cable'
-    listing_description: 'Radisson Blu Polar Hotel is the northernmost full-service hotel in the world, located in Longyearbyen. Clean room, well positioned, enough dark curains to be able to sleep during midnight sun.'
-    listing_poster_url:
+    listing_amenities: 'Free Wi_Fi, TV with cable',
+    listing_description: 'Radisson Blu Polar Hotel is the northernmost full-service hotel in the world, located in Oslo. Clean room, well positioned, enough dark curains to be able to sleep during midnight sun.'
    )
    fifth_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
   fifth_seed.user = user_seed
   fifth_seed.save
 
 
-
   file = URI.open("https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60.jpg")
     sixth_seed =Listing.new(
     listing_name: 'Russkiy',
     listing_address: 'Bergen, Norway',
-    listing_amenities: 'Free Wi_Fi, TV with cable'
-    listing_description: 'Located in Longyearbyen and within 1.2 km of Svalbard Church, Russkiy Dom features a shared lounge, non-smoking rooms, and free WiFi. just quiet, comfortable and clean.'
+    listing_amenities: 'Free Wi_Fi, TV with cable',
+    listing_description: 'Located in Bergen and within 1.2 km of Svalbard Church, Russkiy Dom features a shared lounge, non-smoking rooms, and free WiFi. just quiet, comfortable and clean.'
   )
   sixth_seed.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
   sixth_seed.user = user_seed
